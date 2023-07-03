3f94d47cad4fb1c04f9e05994c9672f46a4f1db6
kfiddle <kenjfiddle@gmail.com>
2023-07-03 10:40:15 -0500

    test push

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react';
 
 const Dropdown = () => {
-  const [dropdown, setDropDown] = useState(false);
+  const [playersClick, setPlayersClick] = useState(false);
+  const [instrumentsClick, setInstrumentsClick] = useState(false);
+  const [gigsClick, setGigsClick] = useState(false);
 
   useEffect(() => {}, []);
 
-  const getSpecificsOnClick = () => {
-    setDropDown = true;
+  const getSpecificsOnClick = (val) => () => {
+    if (val === 'players') {
+      setPlayersClick(true);
+    } else if (val === 'instruments') {
+      setInstrumentsClick(true);
+    } else {
+      setGigsClick(true);
+    }
+
+    console.log('what up');
   };
   return (
     <div>
       <ul>
-        <li onclick={getSpecificsOnClick}>Players</li>
-        <li onclick={getSpecificsOnClick}>Instruments</li>
-        <li onclick={getSpecificsOnClick}>Gigs</li>
+        {/* <li onclick={() => getSpecificsOnClick('players')}>Players</li>
+        <li onclick={() => getSpecificsOnClick('instruments')}>Instruments</li>
+        <li onclick={() => getSpecificsOnClick('gigs')}>Gigs</li> */}
+        <li onClick={getSpecificsOnClick('players')}>Players</li>
+        <li onClick={getSpecificsOnClick('instruments')}>Instruments</li>
+        <li onClick={getSpecificsOnClick('gigs')}>Gigs</li>
       </ul>
     </div>
   );
