698e2255ac42f589d97e9e357845a06a35630abf
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-04 19:58:56 -0400

    got the side navbar displaying properly

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import LogoSVG from './LogoSVG';
 
 function Navbar() {
   return (
     <div className='navbar-container'>
-      <h1 className='navbar-title'>VenueMatch</h1>
-      <h3 className='navbar-greeting'>Hello, Manager</h3>
+      <div className='navbar-header'>
+        <LogoSVG
+          width={150}
+          height={50}
+          fill='#f5f3e8'
+          className='navbar-logo'
+        />
+        <h3 className='navbar-greeting'>Hello, Manager</h3>
+      </div>
+      <div className='navbar-routes'>
+        <button>Venues</button>
+        <button>Gigs</button>
+        <button>Players</button>
+        <button>Instruments</button>
+      </div>
     </div>
   );
 }
