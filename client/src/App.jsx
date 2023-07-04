a8499a771affa3ffc6df7232ea92b1a4c1e29225
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-03 20:09:19 -0400

    done with webpack config stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import {useState, useEffect} from 'react';
 import React from 'react';
-import Header from './components/header';
+import Header from './components/Header.jsx';
 
-import './App.scss';
+// import './App.scss';
 
 const App = () => {
   return <Header />;
--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,48 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./client/src/index.js'],
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'Dist'),
+    path: path.join(__dirname, 'dist'),
   },
-  mode: 'development',
+  devtool: 'eval-source-map',
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules)/,
       },
       {
-        test: /\.(sa|sc|c)ss$/, // styles files
+        test: /\.s?css/, // styles files
         use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, './src/index.html'),
-      inject: false,
+      template: './client/src/index.html',
     }),
   ],
   devServer: {
     host: 'localhost',
-    port: '8080',
+    port: 8080,
+    hot: true,
+    headers: {'Access-Control-Allow-Origin': '*'},
     static: {
       directory: path.resolve(__dirname, 'Dist'),
-      publicPath: 'Dist',
+      publicPath: '/build',
     },
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
     },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
