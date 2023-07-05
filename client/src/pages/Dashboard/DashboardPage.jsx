17d7378a7cfb32551b0ec55fcd471edc9c20b38c
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-05 12:08:27 -0400

    created pages instead of a ton of components

---
+++ client/src/pages/Dashboard/DashboardPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {players, venues, instruments, gigs} from '../../../../server/dummyData';
+import ControlContainer from './components/ControlContainer';
+import InformationDisplay from '../Instruments/components/InformationDisplay';
+import DataContainer from './components/DataContainer';
+
+function DashboardPage() {
+  // =========== DUMMY DATA ============
+  const props = {};
+  props['players'] = players;
+  props['venues'] = venues;
+  props['instruments'] = instruments;
+  props['gigs'] = gigs;
+  // ===================================
+
+  return (
+    <div className='main-container'>
+      <ControlContainer />
+      <InformationDisplay data={props} />
+      <DataContainer data={props} />
+    </div>
+  );
+}
+
+export default DashboardPage;
