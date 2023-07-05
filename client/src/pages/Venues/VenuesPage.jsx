c3a1de1c5745e120ec40c9225bc5962ae07709fa
Ericesposito <eespo93@optonline.net>
2023-07-05 19:33:09 -0400

    started creating new venue page

--- client/src/pages/Venues/VenuesPage.jsx
+++ client/src/pages/Venues/VenuesPage.jsx
@@ -4,7 +4,12 @@ function VenuesPage(props) {
   // TODO: need to map venue state to props
   return (
     <div>
-      <h1>Venues</h1>
+      <h1 className='venue-header'>Venues</h1>
+      <div className='venue-creator'>
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 }
