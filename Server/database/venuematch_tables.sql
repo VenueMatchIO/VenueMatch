64b3860767294f7ab9155d5858866a7a5b924c91
Sebastian Sarmiento <sebastiansarm@gmail.com>
2023-07-05 11:02:44 -0400

    adjusted db files and configured the db

--- Server/database/venuematch_tables.sql
+++ Server/database/venuematch_tables.sql
@@ -1,27 +1,27 @@
-CREATE TABLE Players (
+CREATE TABLE players (
     id SERIAL PRIMARY KEY,
     name VARCHAR
 );
 
-CREATE TABLE Instruments (
+CREATE TABLE instruments (
     id SERIAL PRIMARY KEY,
     name VARCHAR
 );
 
-CREATE TABLE Venues (
+CREATE TABLE venues (
     id SERIAL PRIMARY KEY,
     name VARCHAR
     location VARCHAR
 );
 
-CREATE TABLE Gigs (
+CREATE TABLE gigs (
     id SERIAL PRIMARY KEY,
     name VARCHAR
     venue_id INT,
     FOREIGN KEY (venue.id) REFERENCES Venues(id)
 )
 
-CREATE TABLE Players_to_Instruments (
+CREATE TABLE players_to_Instruments (
     player_id INT,
     instrument_id INT,
     PRIMARY KEY (player_id, instrument_id),
@@ -29,7 +29,7 @@ CREATE TABLE Players_to_Instruments (
     FOREIGN KEY (instrument_id) REFERENCES Instruments(id)
 )
 
-CREATE TABLE Players_to_Gigs (
+CREATE TABLE players_to_Gigs (
     player_id INT,
     gig_id INT,
     PRIMARY KEY (player_id, gig_id),
