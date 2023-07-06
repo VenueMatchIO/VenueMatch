89f60df56db52784689a32e41fd4d6c4591756a5
Ericesposito <eespo93@optonline.net>
2023-07-05 20:24:14 -0400

    continued building out venue page

--- client/src/pages/Venues/VenuesPage.jsx
+++ client/src/pages/Venues/VenuesPage.jsx
@@ -3,11 +3,35 @@ import React from 'react';
 function VenuesPage(props) {
   // TODO: need to map venue state to props
   return (
-    <div>
+    <div className='venue-page'>
       <h1 className='venue-header'>Venues</h1>
       <div className='venue-creator'>
+        <h2 className='add-venue-header'>Add a new Venue</h2>
+        <div className='venue-name'>
+          <input placeholder='venue name goes here' />
+        </div>
+        <div className='venue-location'>
+          <input placeholder='venue location goes here' />
+        </div>
+        <div className='venue-create-btn-div'>
+          <button>Create</button>
+        </div>
+      </div>
+      <div className='venue-page-body'>
+        <div className='venue-page-body-header'>
+          <h2>Name</h2>
+          <div className=''>
+            <h2>Location</h2>
+          </div>
+        </div>
         <div>
-          
+          <ul>
+            <li>Venue</li>
+            <li>Names</li>
+            <li>Would</li>
+            <li>Go</li>
+            <li>Here</li>
+          </ul>
         </div>
       </div>
     </div>
